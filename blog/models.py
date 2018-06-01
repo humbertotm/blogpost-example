a02cd2346008d55912ea3fae51f354c386684779
humbertotm <humberto.htm@gmail.com>
2018-06-01 12:23:50 -0700

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+import datetime
+
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    # This defines how the object will be represented.
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    # This defines how the object will be represented.
+    def __str__(self):
+        return self.post_title
+
+    # States if the blog post was published less than a day ago.
+    def published_today(self):
+        """Returns True if the blog post was published less than a day ago."""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
